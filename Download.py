8de3f0353ea0c77871b4b59871247c7dfcf95d38
liuweijian <793743423@qq.com>
2017-12-09 21:19:29 +0800

    1.对下载添加用户代理设置功能

--- Download.py
+++ Download.py
@@ -2,19 +2,27 @@
 import urllib2
 
 # 下载页面:
-# 包含错误检测与服务端错误重试（默认两次）
-def download(url, num_retries=2):
+# 错误检测
+# 服务端错误重试（默认两次）
+# 用户代理设置（默认wswp:web scraping with python）
+def download(url, user_agent='wswp', num_retries=2):
     print '下载页面:', url
+    # 设置用户代理
+    headers = {'User-agent': user_agent}
+    request = urllib2.Request(url, headers=headers)
+
     try:
-        html = urllib2.urlopen(url).read()
+        html = urllib2.urlopen(request).read()
+    # 错误检测
     except urllib2.URLError as e:
         print '下载失败：', e.reason
         html = None
-        # 检查是否重试下载
+
+    # 下载重试
         if num_retries > 0:
             # 当http代码是5xx的时候意味着服务端发生错误，重试下载时可能已经被修复
             if hasattr(e, 'code') and 500 <= e.code < 600:
                 return download(url, num_retries-1)
     return html
 
-print download("http://httpstat.us/500")
+print download("http://www.baidu.com/")
