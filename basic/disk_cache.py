6333ea8b1782511d1d7c4bdcf2ca978a1fec01a9
liuweijian <793743423@qq.com>
2017-12-20 18:44:50 +0800

    磁盘缓存添加__getitem__ & __setitem__方法

--- basic/disk_cache.py
+++ basic/disk_cache.py
@@ -3,6 +3,7 @@
 import re
 import os
 import urlparse
+import pickle
 
 class DiskCache:
     """利用文件系统缓存
@@ -27,5 +28,26 @@ class DiskCache:
         filename = "/".join([segment[:255] for segment in filename.split('/')])
         # 组合路径返回
         return os.path.join(self.cache_dir, filename)
+
+    # 获取缓存数据
+    def __getitem__(self, url):
+        """以url为线索从磁盘获取数据"""
+        path = self.url_to_path(url)
+        if os.path.exists(path):
+            with open(path, 'rb') as fp:
+                return pickle.load(fp)
+        # 不存在文件则抛出异常
+        else:
+            raise KeyError(url + ' 文件不存在')
+
+    # 设置缓存数据
+    def __setitem__(self, url, result):
+        """以url为线索向磁盘获存数据"""
+        path = self.url_to_path(url)
+        folder = os.path.dirname(path)
+        if not os.path.exists(folder):
+            os.makedirs(folder)
+        with open(path, 'wb') as fp:
+            fp.write(pickle.dumps(result))
 
 print DiskCache().url_to_path('http://example.webscraping.com/index?adb=a')
